e0ac13c599f78093768ecbb3fc4da659938336b0
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-02-14 14:00:53 +0200

    Create Codeforces.cpp

---
+++ Solutions/Codeforces.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI acos(-1)
+#define MOD 1e9+7
+#define SET(a , x) memset(a , x , sizeof(a))
+#define Fast ios_base::sync_with_stdio(false) , cin.tie(0) , cout.tie(0)
+
+
+
+int main(){
+    Fast;
+    int n;
+    cin >> n;
+    int a[n];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    return 0;
+}
