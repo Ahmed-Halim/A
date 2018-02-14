fb4fa6dd25f70e0374c12b7402d9e99cbefab943
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-02-14 14:38:28 +0200

    Update Codeforces.cpp

--- Solutions/Codeforces.cpp
+++ Solutions/Codeforces.cpp
@@ -6,17 +6,36 @@ using namespace std;
 #define SET(a , x) memset(a , x , sizeof(a))
 #define Fast ios_base::sync_with_stdio(false) , cin.tie(0) , cout.tie(0)
 
-
-
 int main(){
     Fast;
-    int n;
-    cin >> n;
-    int a[n];
+    long long n , m;
+    cin >> n >> m;
+    long long a[n] , b[m];
     
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+    long long Max = 0 , Index;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i]*b[j] > Max) {
+                Max =a[i]*b[j] ;
+                Index = i;
+            }
+        }
+    }
+    Max = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i != Index && a[i]*b[j] > Max) {
+                Max = a[i]*b[j] ;
+            }
+        }
+    }
+    cout << Max;
     return 0;
 }
