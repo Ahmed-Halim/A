3aac875ed47e59751795c3df8827c7ff0bfa2182
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-02-17 13:29:50 +0200

    Update Codeforces.cpp

--- Solutions/Codeforces.cpp
+++ Solutions/Codeforces.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    long long n , k , ans = 0 , o = 1 , Min = INT_MAX;
-    
-    cin >> n >> k;
-    long long a[k];
-    for (int i = 0; i < k; i++) {
-        cin >> a[i];
-        if (n%a[i] < Min) {
-            Min =n%a[i];
-            o = i+1;
-            ans = n/a[i];
+int f (int a[] , int length , int size) {
+    int sum[size] , l = 0 , r;
+    sum[0] = a[0];
+    int Max = 0;
+    for (int i = 1; i < size; i++) {
+        sum[i] = sum[i-1] + a[i];
+        if (i >= length) {
+            if (sum[i] - sum[i-length] > Max) {
+                Max = sum[i] - sum[i-length];
+                l = i-length+1;
+            }
         }
     }
-    cout << o << " " << ans;
+    return l;
+}
+using namespace std;
+int main() {
+    int n = 11;
+    int arr[11] = {4,6,7,2,5,3,45,26,3,99,7};
+    cout << f (arr , 2 , n);
     
     return 0;
 }
