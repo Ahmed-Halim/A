b68f3fa1311f4f2f05f8d15657432db463ea7d30
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-02-17 12:21:23 +0200

    Update Codeforces.cpp

--- Solutions/Codeforces.cpp
+++ Solutions/Codeforces.cpp
@@ -1,43 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PI acos(-1)
-#define MOD 1e9+7
-#define SET(a , x) memset(a , x , sizeof(a))
-#define Fast ios_base::sync_with_stdio(false) , cin.tie(0) , cout.tie(0)
-
-int Sum (int num) {
-    int sum = 1;
-    while (num) {
-        sum *= num%10 == 0 ? 1 : num%10;
-        num /= 10;
-    }
-    if (sum >= 10) return Sum(sum);
-    return sum;
-}
 int main(){
-    Fast;
-    map <int , vector <int>> mp;
-    for (int i = 1; i <= 1000000; i++) {
-        int temp = Sum(i);
-        if (temp >= 1 && temp <= 9) {
-            mp[temp].push_back(i);
+    long long n , k , ans = 0 , o = 1 , Min = INT_MAX;
+    
+    cin >> n >> k;
+    long long a[k];
+    for (int i = 0; i < k; i++) {
+        cin >> a[i];
+        if (n%a[i] < Min) {
+            Min =n%a[i];
+            o = i+1;
+            ans = n/a[i];
         }
     }
-    
- 
-    
-    int Q , l , r , k , count = 0;
-    cin >> Q;
-    
-    for (int i = 0; i < Q; i++) {
-        cin >> l >> r >> k;
-        vector<int>::iterator lower,up;
-        low=std::lower_bound (mp[k].begin(), mp[k].end(), l);
-        up= std::upper_bound (mp[k].begin(), mp[k].end(), r);
-        cout << up - lower << endl;
-    }
-    
+    cout << o << " " << ans;
     
     return 0;
 }
