897f5322323a05942187515c193b96b74cb5441a
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-02-19 18:51:07 +0200

    Update Codeforces.cpp

--- Solutions/Codeforces.cpp
+++ Solutions/Codeforces.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 int main(){
-    int n , ans = 0;
+    int n , x = 0 , y = 0 , ans = 0;
+    string dir;
     cin >> n;
-    for (int i = 1; i < n; i++) {
-        if ((n-i) % i == 0) {
+    cin >> dir;
+    for (int i = 0; i < n; i++) {
+        if (dir[i] == 'U') {
+            x++;
+        }
+        if (dir[i] == 'R') {
+            y++;
+        }
+        
+        if (x == y && dir[i] == dir[i+1]) {
             ans++;
         }
     }
